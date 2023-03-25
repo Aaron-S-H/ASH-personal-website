26787f554d78176666f30d5629a65bcf17cb2ad5
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-03-25 17:00:52 +0100

    event listener, css, attempts

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1 = document.getElementsByTagName("h1")[0];
+h1.remove();
+
+const backgroundMain = document.getElementById("backgroundMain");
+
+const backgroundClick = document.getElementById("backgroundClick");
+
+const input = document.getElementByTagName(div);
+
+
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
