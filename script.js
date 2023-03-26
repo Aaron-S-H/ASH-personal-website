910bd5b6e8e90072a43b0cbe24c6511e0bfa2f76
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-03-26 01:00:03 +0100

    Committing working event listener and background sizing

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 const h1 = document.getElementsByTagName("h1")[0];
 h1.remove();
 
-const backgroundMain = document.getElementById("backgroundMain");
-
-const backgroundClick = document.getElementById("backgroundClick");
-
-const input = document.getElementByTagName(div);
-
-
+const input = document.getElementById('backgroundMain');
 input.addEventListener('click', function() {
   alert('I was clicked!');
 });
